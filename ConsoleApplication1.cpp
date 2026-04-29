2ebcf07154169b82d458d4b8ba7aaa75255e1b04
Fabu Ruso <110607502+FabuRuso@users.noreply.github.com>
2026-04-29 09:10:42 +0300

    Clearing of inclusions

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,9 +1,6 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <Windows.h>
-#include <fcntl.h>
-#include <io.h>
 #include <thread>
-#include <string>
 
 #include "Volumizer.h"
 
@@ -18,7 +15,7 @@ void inputeventer(bool* Running) {
 
 	float f, s;
 	bool Occup = true;
-	while (true) {
+	while (*Running) {
 		f = 0;
 		s = 0;
 		if (Occup)
@@ -47,15 +44,6 @@ void inputeventer(bool* Running) {
 	}
 }
 
-void SetCursorVisible(bool visible) {
-	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-	CONSOLE_CURSOR_INFO CCI;
-
-	GetConsoleCursorInfo(consoleHandle, &CCI);
-	CCI.bVisible = visible;
-	SetConsoleCursorInfo(consoleHandle, &CCI);
-}
-
 wchar_t GetBrightChar(float _V) {
 	wchar_t Res = L' ';
 	if (_V >= 0.95)
@@ -69,16 +57,63 @@ wchar_t GetBrightChar(float _V) {
 	return Res;
 }
 
-void SetFontSize(HANDLE h, Seltrad::INT::uint8t Height) {
+using namespace Seltrad::INT;
+
+void SetCursorVisible(HANDLE _V, bool _Visible) {
+	CONSOLE_CURSOR_INFO CCI;
+
+	GetConsoleCursorInfo(_V, &CCI);
+	CCI.bVisible = _Visible;
+	SetConsoleCursorInfo(_V, &CCI);
+}
+
+void SetFontSize(HANDLE _H, uint8t _Height) {
 	CONSOLE_FONT_INFOEX CFI = {};
 	CFI.cbSize = sizeof(CFI);
-	GetCurrentConsoleFontEx(h, 0, &CFI);
-	CFI.dwFontSize = { 0, Height};
+	GetCurrentConsoleFontEx(_H, 0, &CFI);
+	CFI.dwFontSize = { 0, _Height};
 	wcscpy_s(CFI.FaceName, L"Consolas");
 	bool check;
-	check = SetCurrentConsoleFontEx(h, 0, &CFI);
-	if (!check)
-		wprintf(L"%i %i\n", check, GetLastError());
+	SetCurrentConsoleFontEx(_H, 0, &CFI);
+}
+
+void SetCursorVisibleM(HANDLE* _Hs, uint8t _Amount, bool _Visible) {
+	for (uint8t i = 0; i < _Amount; i++) {
+		SetCursorVisible(_Hs[i], _Visible);
+	}
+}
+
+void SetFontSizeM(HANDLE* _Hs, uint8t _Amount, uint8t _Height) {
+	for (uint8t i = 0; i < _Amount; i++) {
+		SetFontSize(_Hs[i], _Height);
+	}
+}
+
+void UpdateHM(HANDLE* _Hs) {
+	SetConsoleActiveScreenBuffer(_Hs[0]);
+}
+
+void UpdateCurrentHandle(HANDLE* _Hs, uint8t _Amount, wchar_t* _Screen, uint32t _ScreenSize, bool* _Running, clock_t* _Time) {
+	{
+		clock_t start, end;
+		while (*_Running)
+		{
+			start = clock();
+
+			HANDLE* Temp = new HANDLE[_Amount];
+			for (uint8t i = 1; i < _Amount; i++) {
+				Temp[i - 1] = _Hs[i];
+			}
+			Temp[_Amount - 1] = _Hs[0];
+			uint64lt written;
+			WriteConsoleOutputCharacterW(_Hs[0], _Screen, _ScreenSize, { 0,0 }, &written);
+			_Hs = Temp;
+			Sleep(5);
+
+			end = clock();
+			*_Time = end - start;
+		}
+	}
 }
 
 int main()
@@ -86,15 +121,16 @@ int main()
 	bool Running = true;
 	std::thread Inputer{ inputeventer, &Running };
 
+	uint8t HandleAmount = 3;
 	HANDLE handle = GetStdHandle(-11);
+	HANDLE* HandleArr = new HANDLE[HandleAmount];
+	HandleArr[0] = GetStdHandle(-11);
+	for (uint8t i = 1; i < HandleAmount; i++)
+		HandleArr[i] = CreateConsoleScreenBuffer(GENERIC_ALL, 0, 0, CONSOLE_TEXTMODE_BUFFER, 0);
 
-	SetCursorVisible(false);
+	SetCursorVisibleM(HandleArr, HandleAmount, false);
 
-	SetConsoleCP(65001);
-	SetConsoleOutputCP(65001);
-	_setmode(_fileno(stdout), _O_U16TEXT);
-
-	SetFontSize(handle, 2);
+	SetFontSizeM(HandleArr, HandleAmount, 2);
 
 	keybd_event(VK_F11, NULL, NULL, 0);
 
@@ -103,26 +139,26 @@ int main()
 	CONSOLE_SCREEN_BUFFER_INFO CSBI;
 	GetConsoleScreenBufferInfo(handle, &CSBI);
 	COORD ScreenSize = CSBI.dwMaximumWindowSize;
-	ScreenSize.X -= 2;
-	ScreenSize.Y -= 2;
+	ScreenSize.X -= 0;
+	ScreenSize.Y -= 0;
 	COORD ImageSize = { MAIN.MainPlayer.getResX(),MAIN.MainPlayer.getResY() };
 	SetConsoleScreenBufferSize(handle, ScreenSize);
 
-	wchar_t** Screen = new wchar_t* [ScreenSize.Y];
-	for (size_t y = 0; y < ScreenSize.Y; y++) {
-		Screen[y] = new wchar_t[ScreenSize.X + 2];
-	}
-	std::wstring ganz = L"";
+	wchar_t* Screen = new wchar_t[ScreenSize.Y * ScreenSize.X];
 
 	clock_t end, start, Engining, Consoling;
 	clock_t
 		FrameStart = 0,
-		FrameEnd = 0;
-	float
-		FPS = 0;
+		FrameEnd = 0,
+		Frame = 0;
+
+	clock_t ThreadFrame;
+	std::thread Updater{ [&HandleArr, HandleAmount, &Screen, &ScreenSize, &Running, &ThreadFrame]() {
+		UpdateCurrentHandle(HandleArr, HandleAmount, Screen, ScreenSize.X * ScreenSize.Y, &Running, &ThreadFrame);
+		} };
+
 	while (Running) {
 		FrameStart = clock();
-		ganz = L"";
 		start = clock();
 		auto Raw = MAIN.GetBrightScheme();
 		end = clock();
@@ -130,8 +166,6 @@ int main()
 		Engining = end - start;
 
 		start = clock();
-		//_flushall();
-		//SetConsoleCursorPosition(handle, { 0,0 });
 		for (size_t Sy = 0; Sy < ScreenSize.Y; Sy++) {
 			for (size_t Sx = 0; Sx < ScreenSize.X; Sx++) {
 				float
@@ -142,14 +176,10 @@ int main()
 				int ix = (int)(Sx * ImageX / ScreenX);
 				int iy = (int)(Sy * ImageY / ScreenY);
 				int index = iy * ImageX + ix;
-				Screen[Sy][Sx] = GetBrightChar(Raw[index]);
+				Screen[Sy * ScreenSize.X + Sx] = GetBrightChar(Raw[index]);
 			}
-			Screen[Sy][ScreenSize.X] = '\n';
-			Screen[Sy][ScreenSize.X + 1] = 0;
-			ganz += Screen[Sy];
 		}
-		//wprintf(ganz.c_str());
-		WriteConsoleW(handle, ganz.c_str(), ganz.length(), nullptr, nullptr);
+		UpdateHM(HandleArr);
 		end = clock();
 		Consoling = end - start;
 
@@ -167,15 +197,24 @@ int main()
 			FPS
 		);*/
 		FrameEnd = clock();
-		FPS = 1.f / (1.f * FrameEnd - FrameStart) * 1000;
+		Frame = FrameEnd - FrameStart;
 	}
-	SetConsoleCursorPosition(handle, { 0,0 });
-	SetFontSize(handle, 16);
+
+	Inputer.join();
+	Updater.join();
+
+
+	SetConsoleCursorPosition(GetStdHandle(-11), { 0,0 });
+	SetFontSize(GetStdHandle(-11), 16);
 	Sleep(100);
 	wprintf(
-		L"FPSonExit: %0.2f\n"
-		L"EnginePSonExit: %i\n"
-		L"ConsolePSonExit: %i\n",
-		FPS, Engining, Consoling
+		L"   Inner time per Frame upon Exit: %04i ms | %02.2f\n"
+		L"               EnginePS upon Exit: %04i ms | %02.2f %%\n"
+		L"              ConsolePS upon Exit: %04i ms | %02.2f %%\n"
+		L"Thread's time per Frame upon Exit: %04i ms | %02.2f\n",
+		Frame, 1.f / Frame * 1000.f,
+		Engining, 100.f * Engining / Frame,
+		Consoling, 100.f * Consoling / Frame,
+		ThreadFrame, 1.f / ThreadFrame * 1000.f
 	);
 }
